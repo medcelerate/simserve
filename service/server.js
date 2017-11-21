25cc92074381a609ed65c3ff55c355346a084f80
Your Name <you@example.com>
2017-11-21 10:47:08 -0500

    Updates

--- service/server.js
+++ service/server.js
@@ -14,12 +14,10 @@ server.get('/data', (req, res, next) => {
 server.post('/post', (req, res, next) => {
     let output, input, model;
     
-    if (fs.existsSync('/tmp/output')) {
+    if (!fs.existsSync('/tmp/output')) {
         fs.mkdirSync('/tmp/output')
-    } else {
-        continue
     }
-    output = '/tmp' + '/ouput/' + Math.floor(Math.random() * 1000000000);
+    output = '/tmp' + '/output/' + Math.floor(Math.random() * 1000000000);
     fs.mkdirSync(output)
     input = req.files.image.path
     let dirName = Math.floor(Math.random() * 1000000000);
@@ -27,13 +25,14 @@ server.post('/post', (req, res, next) => {
     fs.mkdirSync('/tmp/' + dirName)
     dirName = dirName + '/' + req.files.image.name
     console.log(dirName)
+    let oname = req.files.image.name.split('.png')[0]
     fs.renameSync(input, '/tmp/'+ dirName)
     let spawn = require('child_process').spawn;
     let argu = ['/home/mpcr/pix2pix-tensorflow/pix2pix.py', '--mode test', '--output_dir', output, '--input_dir', inputDir, '--checkpoint', model]
     let process = spawn('python', argu);
 
     process.stdout.on('exit', function (data) {
-        let html = fs.readFileSync(output + '/index.html')
+        let html = fs.readFileSync(output + '/images/' + oname + '-outputs.png')
         res.send(200, html)
       });
     })
